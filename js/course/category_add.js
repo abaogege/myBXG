b94c56a08bee22415cad300a97c469066b63a094
abao <363889327@qq.com>
2017-05-17 14:00:45 +0800

    #9
    课程板块下的所有页面的逻辑

--- js/course/category_add.js
+++ js/course/category_add.js
@@ -1,21 +1,73 @@
-define(['jquery','tool','nprogress','aside','header'],
-function ($,abaoTools,NProgress) { 
-    //确认是否登录并处理
-    abaoTools.jumpToLogin();
+define(['jquery', 'tool', 'nprogress', 'template', 'aside', 'header', 'jqueryForm'],
+    function ($, abaoTools, NProgress, temp) {
+        //确认是否登录并处理
+        abaoTools.jumpToLogin();
 
-    //nprogress渲染
-    NProgress.start();
-    setTimeout(function () {
-        NProgress.done();
-    }, 1000);
-
-
-    //loading处理
-    abaoTools.showLoading();
-    abaoTools.hideLoading();
-
-    // 设置下拉框
-    $("#cs-manager-list").show()
+        //nprogress渲染
+        NProgress.start();
+        setTimeout(function () {
+            NProgress.done();
+        }, 1000);
 
 
- })
+        //loading处理
+        abaoTools.loading();
+
+        // 设置下拉框
+        $("#cs-manager-list").show()
+
+        //判断是添加还是编辑
+        //添加
+        if (!location.search) {
+
+            $("#cg-manager-form").ajaxForm({
+                success: function (data) {
+                    console.log(data);
+                }
+            })
+        }
+        //编辑
+        if (location.search) {
+            $.ajax({
+                url: "/v6/category/edit",
+                type: "get",
+                data: {
+                    "cg_id": location.search.slice(1),
+                },
+                success: function (data) {
+                    console.log(data);
+                    $("#cg-manager-box").html(temp("cg-manager-temp", data.result))
+                    //点击保存，发送数据
+                    $("#cg-manager-form").ajaxForm({
+                        success: function (data) {
+                            console.log(data);
+                        }
+                    })
+                }
+            })
+        }
+        //获取顶级
+        function getTop() {
+            $.ajax({
+                url: "/v6/category/top",
+                type: "post",
+                success: function (data) {
+
+                    $("#cg-manager-top-box").html(temp("cg-manager-top", data));
+                }
+            })
+        }
+
+        function getChilds(cg_id) {
+            $.ajax({
+                data: {
+                    "cg_id": cg_id
+                },
+                url: "/v6/category/child",
+                type: "post",
+                success: function () {
+
+                }
+            })
+        }
+    })
--- js/course/category_list.js
+++ js/course/category_list.js
@@ -1,20 +1,51 @@
-define(['jquery','tool','nprogress','aside','header'],
-function ($,abaoTools,NProgress) { 
-    //确认是否登录并处理
-    abaoTools.jumpToLogin();
+define(['jquery', 'tool', 'nprogress', 'template', 'aside', 'header'],
+    function ($, abaoTools, NProgress, temp) {
+        //确认是否登录并处理
+        abaoTools.jumpToLogin();
 
-    //nprogress渲染
-    NProgress.start();
-    setTimeout(function () {
-        NProgress.done();
-    }, 1000);
-
-
-    //loading处理
-    abaoTools.loading();
-
-    // 设置下拉框
-    $("#cs-manager-list").show()
+        //nprogress渲染
+        NProgress.start();
+        setTimeout(function () {
+            NProgress.done();
+        }, 1000);
 
 
- })
+        //loading处理
+        abaoTools.loading();
+
+        // 设置下拉框
+        $("#cs-manager-list").show();
+
+        // //获取数据渲染页面
+        // $.ajax({
+        //     url: "/v6/category/top",
+        //     type: "get",
+        //     success: function (data) {
+        //         console.log(data);
+        //         $("#cg-list").html(temp("cg-list-top-temp", data));
+        //         $("tr[class=active]").each(function (index, ele) {
+        //             $.ajax({
+        //                 url: "/v6/category/child",
+        //                 type: "get",
+        //                 data: {
+        //                     cg_id: $(ele).find("a").attr("data-id")
+        //                 },
+        //                 success: function (data) {
+        //                     console.log(data);
+        //                     $(ele).after(temp("cg-list-childs-temp", data));
+        //                 }
+        //             })
+        //         })
+        //     }
+        // });
+
+        $.ajax({
+            url:"/v6/category",
+            type:"post",
+            success:function (data) {  
+                console.log(data);
+                $("#cg-list").html(temp("cg-list-top-temp", data));
+            }
+        })
+
+    })
